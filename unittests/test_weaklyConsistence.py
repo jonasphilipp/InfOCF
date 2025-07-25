25ca5d4e3385dd7becef6e50ba8decc39496a23f
Arthur Sanin <sanin.arthur@gmail.com>
2025-07-25 11:57:04 +0200

    add test for when the knowledge base is not even weakly consistent

---
+++ unittests/test_weaklyConsistence.py
@@ -0,0 +1,37 @@
+import os
+import sys
+import unittest
+import pandas as pd
+from parser.Wrappers import parse_belief_base, parse_queries
+from inference.inference_operator import InferenceOperator
+from inference.consistency_sat import consistency,consistency_indices
+
+class InferenceCorrectnessTest(unittest.TestCase):
+    def test_weakly_inconsistent(self):
+        weaklyInconsistentCKB= """
+        signature
+            a,b,c
+
+        conditionals 
+        empty{
+            (Bottom|Top),
+            (Bottom|Top),
+            (Bottom|Top),
+            (Bottom|Top),
+            (Bottom|Top),
+            (Bottom|Top),
+            (Bottom|Top),
+            (Bottom|Top)
+            
+        }
+        """
+        ckb = parse_belief_base(weaklyInconsistentCKB)
+        a,b = consistency(ckb, 'z3', True)
+        ai,bi = consistency_indices(ckb, 'z3', True)
+        assert(a==False)
+        assert(b==False)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ unittests/weaklyInconsistent.cl
@@ -0,0 +1,19 @@
+
+
+"""
+signature
+	a,b,c
+
+conditionals 
+empty{
+	(Bottom|Top),
+	(Bottom|Top),
+	(Bottom|Top),
+	(Bottom|Top),
+	(Bottom|Top),
+	(Bottom|Top),
+	(Bottom|Top),
+	(Bottom|Top)
+	
+}
+"""
