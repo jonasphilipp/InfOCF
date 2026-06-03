83db5e2826d45639a612a4febc085ed0cc75d1a9
Arthur Sanin <sanin.arthur@gmail.com>
2026-06-03 18:16:20 +0200

    bug fixes

--- generate_benchmark.sh
+++ generate_benchmark.sh
@@ -1,3 +1,3 @@
 
 
-taskset -c 0 python3 weakly_generator.py 1 | taskset -c 1 python3 weakly_generator.py 2 | taskset -c 0 python3 weakly_generator.py 3 
+taskset -c 0 python3 weakly_generator.py 1 | taskset -c 1 python3 weakly_generator.py 2 | taskset -c 2 python3 weakly_generator.py 3 
